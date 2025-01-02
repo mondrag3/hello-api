6ac885457f7b3ec4be997d05c4ec4ff17d95aa10
Matt Mondragon <mondrag3@users.noreply.github.com>
2025-01-02 04:27:31 +0000

    test: add pytest and tox for testing and automation
    - Set up pytest for writing and running unit tests
    - Configure tox for test automation across environments
    - Include test-specific requirements in requirements-test.txt
    - Add sample pytest for the ping endpoint

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from fastapi import FastAPI
 # Create an instance of FastAPI
 app = FastAPI()
 
+
 # Define the ping endpoint
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     # Run the application
     uvicorn.run(app, host="127.0.0.1", port=8000)
